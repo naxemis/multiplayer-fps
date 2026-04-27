3a9d442dbd0e1881051980f0930de59f332304a3
naxemis <szymon.lenard@icloud.com>
2026-04-27 14:58:49 +0200

    refactor(state_machine): create plain state machine script

---
+++ scripts/player/components/movement_state_machine.gd
@@ -0,0 +1,22 @@
+class_name MovementStateMachine
+extends Node
+
+# Signals
+
+# Enums and constants
+
+# @export vars
+
+# Public vars
+
+# Private vars (_)
+
+# @onready vars
+
+# _init / _ready
+
+# Engine callbacks (_process, _physics_process, _input, _unhandled_input, etc.)
+
+# Public methods (component APIs)
+
+# Private methods (_)
