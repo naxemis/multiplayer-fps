39e108c0a7da5e0e5726f9faefa1cd874274ef8b
naxemis <szymon.lenard@icloud.com>
2026-05-11 19:45:57 +0200

    refactor(player): drive collision animator by state signals

--- scripts/player/components/collision_animator.gd
+++ scripts/player/components/collision_animator.gd
@@ -20,6 +20,8 @@ var _player_context_module: PlayerContextModule
 var _state_machine: StateMachine
 var _animation_tree: AnimationTree
 var _collision_blend_amount: float = 0.0
+var _target_blend_amount: float = 0.0
+var _target_blend_speed: float = 0.0
 
 # @onready vars
 
@@ -34,18 +36,28 @@ func pass_player_context_module(player_context: PlayerContextModule) -> void:
 	_state_machine = player_context.components.state_machine
 	_animation_tree = _player_context_module.node_refs.collision_animation_tree
 
+	_state_machine.state_changed.connect(_on_state_changed)
+	_on_state_changed(_state_machine._current_state)
+
 # Private methods (_)
+func _on_state_changed(new_state: int) -> void:
+	match new_state:
+		_state_machine.MovementStates.CROUCH:
+			_target_blend_amount = crouch_blend_amount
+			_target_blend_speed = crouch_animation_speed
+		_state_machine.MovementStates.SLIDE:
+			_target_blend_amount = slide_blend_amount
+			_target_blend_speed = slide_animation_speed
+		_:
+			_target_blend_amount = 0.0
+			_target_blend_speed = 0.0 # picked per-frame in _collision_shape_animations based on current blend
+
 func _collision_shape_animations(delta) -> void:
 	_collision_blend_amount = clampf(_collision_blend_amount, 0.0, slide_blend_amount)
-	
-	if _state_machine._current_state == _state_machine.MovementStates.CROUCH:
-		_collision_blend_amount = lerpf(_collision_blend_amount, crouch_blend_amount, crouch_animation_speed * delta)
-	elif _state_machine._current_state == _state_machine.MovementStates.SLIDE:
-		_collision_blend_amount = lerpf(_collision_blend_amount, slide_blend_amount, slide_animation_speed * delta)
-	else:
-		if _collision_blend_amount <= crouch_blend_amount:
-			_collision_blend_amount = lerpf(_collision_blend_amount, 0.0, crouch_animation_speed * delta)
-		elif _collision_blend_amount > crouch_blend_amount:
-			_collision_blend_amount = lerpf(_collision_blend_amount, 0.0, slide_animation_speed * delta)
-	
+
+	var speed: float = _target_blend_speed
+	if _target_blend_amount == 0.0:
+		speed = crouch_animation_speed if _collision_blend_amount <= crouch_blend_amount else slide_animation_speed
+
+	_collision_blend_amount = lerpf(_collision_blend_amount, _target_blend_amount, speed * delta)
 	_animation_tree["parameters/State Blend/blend_amount"] = _collision_blend_amount
